f94c4c2450630e58f5f4dc2da3978368867b2a10
Mercy Auma <aumagabriella@gmail.com>
2021-11-07 22:56:04 +0300

    Added some changes to the artist's sub pages

---
+++ scripts/scripts_musicians.js
@@ -0,0 +1,91 @@
+const ClickImage = document.getElementById('clickableImage1');
+const ClickImage2 = document.getElementById('clickableImage2');
+const ClickImage3 = document.getElementById('clickableImage3');
+const ClickImage4 = document.getElementById('clickableImage4');
+const ClickImage5 = document.getElementById('clickableImage5');
+const ClickImage6 = document.getElementById('clickableImage6');
+const ClickImage7 = document.getElementById('clickableImage7');
+const ClickImage8 = document.getElementById('clickableImage8');
+
+// Several inTimes
+
+const inTime = document.getElementById('popDetail_1');
+const inTime2 = document.getElementById('popDetail_2');
+const inTime3 = document.getElementById('popDetail_3');
+const inTime4 = document.getElementById('popDetail_4');
+const inTime5 = document.getElementById('popDetail_5');
+const inTime6 = document.getElementById('popDetail_6');
+const inTime7 = document.getElementById('popDetail_7');
+const inTime8 = document.getElementById('popDetail_8');
+
+ClickImage.onclick = function musicBands_1(){
+
+    if(inTime.style.display !== 'none'){
+        inTime.style.display = 'none';
+    } else {
+        inTime.style.display = 'block'
+    }
+}
+
+ClickImage2.onclick = function musicBands_2(){
+
+    if(inTime2.style.display !== 'none'){
+        inTime2.style.display = 'none';
+    } else {
+        inTime2.style.display = 'block'
+    }
+}
+
+ClickImage3.onclick = function musicBands_3(){
+
+    if(inTime3.style.display !== 'none'){
+        inTime3.style.display = 'none';
+    } else {
+        inTime3.style.display = 'block'
+    }
+}
+
+ClickImage4.onclick = function musicBands_4(){
+
+    if(inTime4.style.display !== 'none'){
+        inTime4.style.display = 'none';
+    } else {
+        inTime4.style.display = 'block'
+    }
+}
+
+ClickImage5.onclick = function musicBands_5(){
+
+    if(inTime5.style.display !== 'none'){
+        inTime5.style.display = 'none';
+    } else {
+        inTime5.style.display = 'block'
+    }
+}
+
+ClickImage6.onclick = function musicBands_6(){
+
+    if(inTime6.style.display !== 'none'){
+        inTime6.style.display = 'none';
+    } else {
+        inTime6.style.display = 'block'
+    }
+}
+
+ClickImage7.onclick = function musicBands_7(){
+
+    if(inTime7.style.display !== 'none'){
+        inTime7.style.display = 'none';
+    } else {
+        inTime7.style.display = 'block'
+    }
+}
+
+ClickImage8.onclick = function musicBands_8(){
+
+    if(inTime8.style.display !== 'none'){
+        inTime8.style.display = 'none';
+    } else {
+        inTime8.style.display = 'block'
+    }
+}
