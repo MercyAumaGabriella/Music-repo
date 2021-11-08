8be28fb00ee817294f1bfc5827dc8a5bcf7b2fd3
Mercy Auma <aumagabriella@gmail.com>
2021-11-08 21:47:30 +0300

    added some changes

--- scripts/scripts_aboutus.js
+++ scripts/scripts_aboutus.js
@@ -1,5 +1,6 @@
 const sponsor1 = document.getElementById("sponsorDetails_1");
 const sponsor2 = document.getElementById("sponsorDetails_2");
+const Main = document.getElementById("main");
 
 const Image1 = document.getElementById("img_1");
 const Image2 = document.getElementById("img_2");
@@ -8,8 +9,10 @@ Image1.onclick = function showMore_1(){
 
     if(sponsor1.style.display !== 'none'){
         sponsor1.style.display = 'none';
+        Main.style.height = "200vh";
     } else {
         sponsor1.style.display = 'block';
+        Main.style.height = "270vh";
     }
     
 }
@@ -18,7 +21,9 @@ Image2.onclick = function showMore_2(){
 
     if(sponsor2.style.display !== 'none'){
         sponsor2.style.display = 'none';
+        Main.style.height = "200vh";
     } else {
         sponsor2.style.display = 'block';
+        Main.style.height = "270vh";
     }
 }
